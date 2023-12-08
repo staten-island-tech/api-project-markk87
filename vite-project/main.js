c65f6a1332c38634a9349f5f6f4965e55a888f80
markk87 <markk87@nycstudents.net>
2023-12-07 19:35:39 -0500

    asdf

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,13 +1,24 @@
 const URL = `https://fortnite-api.com/v1/map`
+const imageContainer = document.querySelector(".imageContainer")
 
 
 async function getData(URL) {
   try {
-      const response = await fetch(URL);
+    const response = await fetch(URL);
       
-      console.log(response);
-      const data = await response.json();
-      console.log(data);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+
+    const pois = data.data.pois;
+
+    pois.forEach(imageURL => {
+      const imgEl = document.createElement("p");
+      imgEl.src = imageURL;
+      imgEl.alt = "Fortnite Map";
+
+       imageContainer.appendChild(imgEl)
+    });
       
       
      
