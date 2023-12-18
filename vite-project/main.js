a91ce05c763bd345d1b3619d9cfc3b600d6182ec
markk87 <markk87@nycstudents.net>
2023-12-18 17:47:59 -0500

    sdf

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,39 +1,61 @@
-const URL = `https://data.cityofnewyork.us/resource/nc67-uf89.json`
+const URL = `https://data.cityofnewyork.us/resource/nc67-uf89.json` //camera violations
+const crashURL = `https://data.cityofnewyork.us/resource/bm4k-52h4.json` //vehicle thing a ma bob
 const imageContainer = document.querySelector(".imageContainer");
 const plateForm = document.getElementById("plateForm");
 
 
-async function getData(URL) {
+const DOMSelectors = {
+  button: document.querySelector("button")
+}
+
+async function getCrashData(crashURL) {
   try {
-    const response = await fetch(URL);
-      
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
+    const crashResponse = await fetch(crashURL);
+
+    console.log(crashResponse);
+    const crashData = await crashResponse.json();
+    console.log(crashData);
     imageContainer.innerHTML = "";
 
-    data.forEach(el => {
-      const textEl = document.createElement("p");
-      textEl.textContent = `Plate: ${el.plate}, Violation: ${el.violation}, Date: ${el.issue_date}`;
-      imageContainer.appendChild(textEl);
-
-      const button = document.createElement("button");
-      button.textContent = "See More";
-      imageContainer.appendChild(button);
-    });
+    
   } catch (error) {
-    console.log("Sorry, we could not find what you are looking for");
+    console.log("Sorry, it didnt work");
   }
 }
-getData(URL);
 
-plateForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const plateInput = document.getElementById("plateNumber").value;
-  getData(`${URL}?plate=${plateInput}`);
-});
+getCrashData();
 
-button.addEventListener("click", function (){
-  
-})
+// async function getData(URL) {
+//   try {
+//     const response = await fetch(URL);
+      
+//     console.log(response);
+//     const data = await response.json();
+//     console.log(data);
+//     imageContainer.innerHTML = "";
+
+//     data.forEach(el => {
+//       const textEl = document.createElement("p");
+//       textEl.textContent = `Plate: ${el.plate}, Violation: ${el.violation}, Date: ${el.issue_date}`;
+//       imageContainer.appendChild(textEl);
+
+      
+//     });
+//   } catch (error) {
+//     console.log("Sorry, we could not find what you are looking for");
+//   }
+// }
+// getData(URL);
+
+// plateForm.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   const plateInput = document.getElementById("plateNumber").value;
+//   getData(`${URL}?plate=${plateInput}`);
+// });
+
+
+// DOMSelectors.button.addEventListener("click", function(e){
+//   e.preventDefault();
+//   const buttonClick = 
+// })
 
